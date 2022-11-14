e20e229a7b238b2aaec6c9fc351aa55825613d49
Tappobill <christian.salvati@hotmail.it>
2022-11-14 17:41:21 +0100

    Prima Lista

---
+++ jscript/main.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        todoList : [
+            {
+                text: 'Studiare le slides di Boolean'
+            },
+            {
+                text: 'Fare gli esercizi di Boolean'
+            },
+            {
+                text: 'Buttarsi dal balcone a causa di Boolean'
+            }
+        ]
+    }
+})
